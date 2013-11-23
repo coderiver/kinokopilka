ff6c74903ccf83241275a6f45fca78760b8ea407
akella <akella@Yuris-MacBook-Pro.local>
2013-11-23 22:44:49 +0200

    1.2 1.3 1.4

--- js/common.js
+++ js/common.js
@@ -69,4 +69,32 @@ $('.js-recomm input').change(function() {
 $('.btn, .js-tooltip').tooltip();
 $('.carousel').carousel();
 
+
+
+//fix nav
+var stickyPanelSettings = {
+        // Use this to set the top margin of the detached panel.
+        topPadding: 0,
+        // This class is applied when the panel detaches.
+        afterDetachCSSClass: "",
+        // When set to true the space where the panel was is kept open.
+        savePanelSpace: true,
+        // Event fires when panel is detached
+        // function(detachedPanel, panelSpacer){....}
+        onDetached: null,
+        // Event fires when panel is reattached
+        // function(detachedPanel){....}
+        onReAttached: null,
+        // Set this using any valid jquery selector to 
+        // set the parent of the sticky panel.
+        // If set to null then the window object will be used.
+        parentSelector: null
+    };
+    if($(window).width()>600){
+    	$(".nav-wrap").stickyPanel(stickyPanelSettings);
+    }
+    
+
+
+
 });
